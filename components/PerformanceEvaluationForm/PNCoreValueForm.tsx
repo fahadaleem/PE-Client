bf2144c93ac908ff0f65cd621baf76f46ffef6c3
nomadech-solutions <nomadech.solutions@gmail.com>
2022-04-26 23:22:00 +0500

    fix.

--- components/PerformanceEvaluationForm/PNCoreValueForm.tsx
+++ components/PerformanceEvaluationForm/PNCoreValueForm.tsx
@@ -25,45 +25,6 @@ export const PNCoreValueForm = (props) => {
 
   // const[questions,setquestions]=useState([])
 
-  const [questions, setquestions] = useState([
-    {
-      questionTitle: "Test Question 1",
-      questionId: 1,
-      questionDescription: "Progress Of Student",
-      options: [
-        { optionTitle: "Option2", optionWeightage: "1" },
-        { optionTitle: "Option3", optionWeightage: "2" },
-        { optionTitle: "Option4", optionWeightage: "3" },
-        { optionTitle: "Option5", optionWeightage: "4" },
-      ],
-    },
-    {
-      questionTitle: "Test Question 2",
-      questionId: 2,
-      questionDescription: "Progress Of Student 1",
-      options: [
-        {
-          optionTitle: "Option1",
-          optionWeightage: "5",
-        },
-        { optionTitle: "Option2", optionWeightage: "1" },
-        { optionTitle: "Option3", optionWeightage: "2" },
-        { optionTitle: "Option4", optionWeightage: "3" },
-        { optionTitle: "Option5", optionWeightage: "4" },
-      ],
-    },
-    {
-      questionTitle: "Test Question 3",
-      questionId: 3,
-      questionDescription: "Progress Of Student",
-      options: [
-        { id: 2, optionTitle: "Option2", optionWeightage: "1" },
-        { id: 3, optionTitle: "Option3", optionWeightage: "2" },
-        { id: 4, optionTitle: "Option4", optionWeightage: "3" },
-        { id: 5, optionTitle: "Option5", optionWeightage: "4" },
-      ],
-    },
-  ]);
   console.log("props", props);
   // const [scores, setScores] = useState([
   //   {
@@ -81,16 +42,9 @@ export const PNCoreValueForm = (props) => {
       >
         Evaulation Form
       </Heading>
-
-      {props.question.map((title: any, key: any) => {
-        return (
-          <div>
-            <Text color={"rgb(135,144,174)"} size="lg" textAlign={"center"}>
-              {title?.mainSection?.sectionTitle}
-            </Text>
-          </div>
-        );
-      })}
+      <Text color={"rgb(135,144,174)"} size="lg" textAlign={"center"}>
+        {props.question[0]?.mainSection?.sectionTitle}
+      </Text>
       {props.question.map((title, key) => {
         return (
           <>
@@ -102,6 +56,76 @@ export const PNCoreValueForm = (props) => {
                       <Heading as="h3" size="lg" color={"rgb(120,149,246)"}>
                         {sectionData.subSectionTitle}
                       </Heading>
+                      {sectionData?.questions?.map((question, key) => {
+                        return (
+                          <Box>
+                            <Heading as="h3" size="lg" color={"rgb(46,58,115)"}>
+                              <Text>
+                                Ques {key + 1}:{question.questionTitle}
+                              </Text>
+                              <Text fontSize={"md"}>
+                                {question.questionDescription}
+                              </Text>
+                            </Heading>
+                            <Box margin={"20px"}>
+                              <Stack>
+                                <RadioGroup
+                                  onChange={(e) => {
+                                    const data = {
+                                      questionId: question.questionId,
+                                      option: e,
+                                    };
+
+                                    // find the index in evalData
+                                    let myDataIndex = evalData.findIndex(
+                                      (item, key) =>
+                                        item.questionId == question.questionId
+                                    );
+
+                                    // if option not selected then add new option to the data.
+
+                                    if (myDataIndex === -1) {
+                                      setEvalData([...evalData, data]);
+                                      return;
+                                    } else {
+                                      // update the excisting question option.
+                                      let newData = evalData;
+                                      newData.splice(myDataIndex, 1, data);
+                                      setEvalData([...newData]);
+                                    }
+                                  }}
+                                  value={eval[key]?.option}
+                                >
+                                  {question.options.map((optionNo, key) => {
+                                    return (
+                                      <div>
+                                        <Box
+                                          bg={"rgb(229,232,239)"}
+                                          width={"200px"}
+                                          borderRadius={"10px"}
+                                          padding={"5px"}
+                                        >
+                                          <Radio
+                                            style={{
+                                              border:
+                                                "1px solid rgb(76,98,150)",
+                                              boxShadow: "none",
+                                            }}
+                                            colorScheme={"green"}
+                                            value={optionNo.optionWeightage}
+                                          >
+                                            {optionNo.optionTitle}
+                                          </Radio>
+                                        </Box>
+                                      </div>
+                                    );
+                                  })}
+                                </RadioGroup>
+                              </Stack>
+                            </Box>
+                          </Box>
+                        );
+                      })}
                     </Box>
                   </>
                 );
@@ -110,112 +134,110 @@ export const PNCoreValueForm = (props) => {
           </>
         );
       })}
-
-      {props.question.map((question: any, key: any) => {
-        question.mainSection.subSections[key].questions.map(
-          (questionNo: any, key: any) => {
-            console.log(questionNo.questionId);
-            console.log(questionNo.questionDescription);
-            return (
-              <>
-                <Box margin={"20px"}>
-                  <Heading as="h3" size="lg" color={"rgb(46,58,115)"}>
-                    Question{":"}
-                    <p>{questionNo.questionId}</p>
-                  </Heading>
-                  <Text
-                    color={"rgb(46,58,115)"}
-                    mt={"10px"}
-                    fontSize="xl"
-                    fontWeight={700}
-                  >
-                    {/* Question {question.questionId} {" : "} */}
-                    {/* {question.questionDescription} */}
-                  </Text>
-                  <Text>
-                    Question Description{questionNo.questionDescription}
-                  </Text>
-                </Box>
-              </>
-            );
-          }
-        );
-        return (
-          <div key={key}>
-            <Box margin={"20px"}>
-              <Heading as="h3" size="lg" color={"rgb(46,58,115)"}>
-                Question{":"}
-                {
-                  // question.mainSection.subSections[key].questions
-                }
-              </Heading>
-              <Text
-                color={"rgb(46,58,115)"}
-                mt={"10px"}
-                fontSize="xl"
-                fontWeight={700}
-              >
-                {/* Question {question.questionId} {" : "} */}
-                {/* {question.questionDescription} */}
-              </Text>
-            </Box>
-            <Box margin={"20px"}>
-              <Stack>
-                <RadioGroup
-                  onChange={(e) => {
-                    const data = {
-                      // questionId: question.questionId,
-                      // option: e,
-                    };
-
-                    // find the index in evalData
-                    let myDataIndex = evalData.findIndex(
-                      (item, key) => item.questionId == question.questionId
-                    );
-
-                    // if option not selected then add new option to the data.
-
-                    if (myDataIndex === -1) {
-                      setEvalData([...evalData, data]);
-                      return;
-                    } else {
-                      // update the excisting question option.
-                      let newData = evalData;
-                      newData.splice(myDataIndex, 1, data);
-                      setEvalData([...newData]);
-                    }
-                  }}
-                  // value={eval[key]?.option}
-                >
-                  {/* {question.options.map((optionNo, key) => {
-                    return (
-                      <div>
-                        <Box
-                          bg={"rgb(229,232,239)"}
-                          width={"200px"}
-                          borderRadius={"10px"}
-                          padding={"5px"}
-                        >
-                          <Radio
-                            style={{
-                              border: "1px solid rgb(76,98,150)",
-                              boxShadow: "none",
-                            }}
-                            colorScheme={"green"}
-                            value={optionNo.optionWeightage}
-                          >
-                            {optionNo.optionTitle}
-                          </Radio>
-                        </Box>
-                      </div>
-                    );
-                  })} */}
-                </RadioGroup>
-              </Stack>
-            </Box>
-          </div>
-        );
-      })}
     </Box>
   );
 };
+
+//  {
+//    props.question.map((question: any, key: any) => {
+//      question.mainSection.subSections[key].questions.map(
+//        (questionNo: any, key: any) => {
+//          return (
+//            <>
+//              <Box margin={"20px"}>
+//                <Heading as="h3" size="lg" color={"rgb(46,58,115)"}>
+//                  Question{":"}
+//                  <p>{questionNo.questionId}</p>
+//                </Heading>
+//                <Text
+//                  color={"rgb(46,58,115)"}
+//                  mt={"10px"}
+//                  fontSize="xl"
+//                  fontWeight={700}
+//                >
+//                  {/* Question {question.questionId} {" : "} */}
+//                  {/* {question.questionDescription} */}
+//                </Text>
+//                <Text>Question Description{questionNo.questionDescription}</Text>
+//              </Box>
+//            </>
+//          );
+//        }
+//      );
+//      return (
+//        <div key={key}>
+//          <Box margin={"20px"}>
+//            <Heading as="h3" size="lg" color={"rgb(46,58,115)"}>
+//              Question{":"}
+//              {
+//                // question.mainSection.subSections[key].questions
+//              }
+//            </Heading>
+//            <Text
+//              color={"rgb(46,58,115)"}
+//              mt={"10px"}
+//              fontSize="xl"
+//              fontWeight={700}
+//            >
+//              {/* Question {question.questionId} {" : "} */}
+//              {/* {question.questionDescription} */}
+//            </Text>
+//          </Box>
+//          <Box margin={"20px"}>
+//            <Stack>
+//              <RadioGroup
+//                onChange={(e) => {
+//                  const data = {
+//                    // questionId: question.questionId,
+//                    // option: e,
+//                  };
+
+//                  // find the index in evalData
+//                  let myDataIndex = evalData.findIndex(
+//                    (item, key) => item.questionId == question.questionId
+//                  );
+
+//                  // if option not selected then add new option to the data.
+
+//                  if (myDataIndex === -1) {
+//                    setEvalData([...evalData, data]);
+//                    return;
+//                  } else {
+//                    // update the excisting question option.
+//                    let newData = evalData;
+//                    newData.splice(myDataIndex, 1, data);
+//                    setEvalData([...newData]);
+//                  }
+//                }}
+//                // value={eval[key]?.option}
+//              >
+//                {/* {question.options.map((optionNo, key) => {
+//                     return (
+//                       <div>
+//                         <Box
+//                           bg={"rgb(229,232,239)"}
+//                           width={"200px"}
+//                           borderRadius={"10px"}
+//                           padding={"5px"}
+//                         >
+//                           <Radio
+//                             style={{
+//                               border: "1px solid rgb(76,98,150)",
+//                               boxShadow: "none",
+//                             }}
+//                             colorScheme={"green"}
+//                             value={optionNo.optionWeightage}
+//                           >
+//                             {optionNo.optionTitle}
+//                           </Radio>
+//                         </Box>
+//                       </div>
+//                     );
+//                   })} */}
+//              </RadioGroup>
+//            </Stack>
+//          </Box>
+//        </div>
+//      );
+//    });
+//  }
